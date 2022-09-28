5bbbc9e01e99e22d1178215fcf5d6003f2d282fa
Marcel Durgante <info@marceldurgante.dev>
2022-09-28 12:37:55 +0200

    Functions showError and showSuccess created

--- scripts.js
+++ scripts.js
@@ -1,14 +1,26 @@
 const form = document.getElementById('form');
-const username = document.getElementById('username');
+const inputUsername = document.getElementById('username');
 const email = document.getElementById('email');
 const password = document.getElementById('password');
 const password2 = document.getElementById('password2');
 
 // Show input error message
-function showError(param1, param2) {
+function showError(input, message) {
+
+    input.parentElement.className = 'form'
+
     const formControl = input.parentElement;
     formControl.className = 'form-control error';
+
+    const small = formControl.querySelector('small');
+    small.innerText = message;
 };
+
+// Show success outline 
+function showSuccess(input) {
+    const formControl = input.parentElement;
+    formControl.className = 'form-control success';
+}
 
 // Event Listners
 form.addEventListener('submit', function(e) {
