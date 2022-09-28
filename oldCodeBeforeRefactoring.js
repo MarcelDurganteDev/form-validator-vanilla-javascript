96fd79bb65ff1d3e60ab3b706b7c617afa59c977
Marcel Durgante <info@marceldurgante.dev>
2022-09-28 19:30:23 +0200

    deleted if statemens inside form event listener for refactoring it

--- oldCodeBeforeRefactoring.js
+++ oldCodeBeforeRefactoring.js
@@ -29,6 +29,30 @@ function isValidEmail(email) {
 form.addEventListener('submit', function(e) {
     e.preventDefault();
 
-    
+    if(username.value === '') {
+        showError(username, 'Username is required');
+    } else {
+        showSuccess(username)
+    }
+
+    if(email.value === '') {
+        showError(email, 'Email is required');
+    } else if (!isValidEmail(email.value)) {
+        showError(email, 'Email is not valid')
+    } else {
+        showSuccess(email)
+    }
+
+    if(password.value === '') {
+        showError(password, 'Password is required');
+    } else {
+        showSuccess(password)
+    }
+    if(password2.value === '') {
+        showError(password2, 'Password2 is required');
+    } else {
+        showSuccess(password2)
+    }
+
 
 });
